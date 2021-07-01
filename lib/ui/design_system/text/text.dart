f434c7777131714fcee16c4024eace7a29d6cc2d
Crabee <37769573+pqr4579@users.noreply.github.com>
2021-07-02 00:10:10 +0900

    수정 : 디자인시스템 Text 기본속성 추가

--- lib/ui/design_system/text/text.dart
+++ lib/ui/design_system/text/text.dart
@@ -8,6 +8,17 @@ class SubpingText extends StatelessWidget {
   final FontSize size;
   final SubpingFontWeight fontWeight;
   final SubpingColor color;
+  final StrutStyle strutStyle;
+  final TextDirection textDirection;
+  final TextAlign textAlign;
+  final Locale locale;
+  final bool softWrap;
+  final TextOverflow overflow;
+  final int maxLines;
+  final double textScaleFactor;
+  final String semanticsLabel;
+  final TextWidthBasis textWidthBasis;
+  final TextHeightBehavior textHeightBehavior;
 
   static Map<SubpingColor, Color> colorSet = {
     SubpingColor.subping100: Color.fromRGBO(80, 110, 225, 1),
@@ -48,16 +59,36 @@ class SubpingText extends StatelessWidget {
     Key key,
     this.color,
     this.fontWeight,
+    this.strutStyle,
+    this.textAlign,
+    this.textDirection,
+    this.locale,
+    this.softWrap,
+    this.overflow,
+    this.textScaleFactor,
+    this.maxLines,
+    this.semanticsLabel,
+    this.textWidthBasis,
+    this.textHeightBehavior,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return (Text(
-      text,
-      style: TextStyle(
-          fontSize: fontSizeSet[this.size],
-          color: colorSet[this.color],
-          fontWeight: fontWeightSet[this.fontWeight]),
-    ));
+    return (Text(text,
+        style: TextStyle(
+            fontSize: fontSizeSet[this.size],
+            color: colorSet[this.color],
+            fontWeight: fontWeightSet[this.fontWeight]),
+        locale: this.locale,
+        strutStyle: this.strutStyle,
+        textAlign: this.textAlign,
+        textWidthBasis: this.textWidthBasis,
+        textHeightBehavior: this.textHeightBehavior,
+        semanticsLabel: this.semanticsLabel,
+        overflow: this.overflow,
+        softWrap: this.softWrap,
+        textDirection: this.textDirection,
+        maxLines: this.maxLines,
+        textScaleFactor: this.textScaleFactor));
   }
 }
