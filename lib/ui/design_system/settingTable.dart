2f42542866f36fe834d46850aca49f0a16f93b37
Lee Jeong Jin <dlwjdwls6504@gmail.com>
2021-07-01 19:35:45 +0900

    기능..디자인 시스템 Space 및 Text

--- lib/ui/design_system/settingTable.dart
+++ lib/ui/design_system/settingTable.dart
@@ -1,1 +1,30 @@
 enum Size { large, regular, small, tiny, xTiny }
+
+enum SubpingColor {
+  subping100,
+  subping50,
+  subping30,
+  warning100,
+  black100,
+  black80,
+  black60,
+  back20,
+  white100,
+}
+
+enum FontSize {
+  title1,
+  title2,
+  title3,
+  title4,
+  title5,
+  title6,
+  body1,
+  body2,
+  body3,
+  body4,
+  body5,
+  tiny1,
+}
+
+enum SubpingFontWeight { bold, medium, regular }
--- lib/ui/design_system/text/text.dart
+++ lib/ui/design_system/text/text.dart
@@ -1,48 +1,63 @@
-import 'dart:html';
-
+import '../settingTable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/text.dart' as SubText;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Text extends StatelessWidget {
+class SubpingText extends StatelessWidget {
   final String text;
-  final String size;
-  final String fontWeight;
-  final TextStyle textStyle;
-  final TextDirection textDirection;
-  final TextAlign textAlign;
-  final Locale locale;
-  final StrutStyle strutStyle;
-  final String semanticsLabel;
-  final int maxLines;
-  final TextOverflow overflow;
-  final bool softWrap;
-  final TextHeightBehavior textHeightBehavior;
-  final TextWidthBasis textWidthBasis;
-  final double textScaleFactor;
+  final FontSize size;
+  final SubpingFontWeight fontWeight;
+  final SubpingColor color;
 
-  const Text(@required this.text,
-      {@required this.size,
-      Key key,
-      this.fontWeight,
-      this.textStyle,
-      this.textDirection,
-      this.strutStyle,
-      this.textAlign,
-      this.locale,
-      this.softWrap,
-      this.overflow,
-      this.textScaleFactor,
-      this.maxLines,
-      this.semanticsLabel,
-      this.textWidthBasis,
-      this.textHeightBehavior})
-      : super(key: key);
+  static Map<SubpingColor, Color> colorSet = {
+    SubpingColor.subping100: Color.fromRGBO(80, 110, 225, 1),
+    SubpingColor.subping50: Color.fromRGBO(80, 140, 255, 1),
+    SubpingColor.subping30: Color.fromRGBO(180, 209, 255, 1),
+    SubpingColor.warning100: Color.fromRGBO(250, 60, 90, 1),
+    SubpingColor.black100: Color.fromRGBO(20, 20, 20, 1),
+    SubpingColor.black60: Color.fromRGBO(170, 176, 195, 1),
+    SubpingColor.black80: Color.fromRGBO(85, 85, 95, 1),
+    SubpingColor.back20: Color.fromRGBO(246, 246, 247, 1),
+    SubpingColor.white100: Color.fromRGBO(0, 0, 0, 1)
+  };
+
+  static Map<FontSize, double> fontSizeSet = {
+    FontSize.title1: 62.nsp,
+    FontSize.title2: 60.nsp,
+    FontSize.title3: 52.nsp,
+    FontSize.title4: 46.nsp,
+    FontSize.title5: 42.nsp,
+    FontSize.title6: 36.nsp,
+    FontSize.body1: 32.nsp,
+    FontSize.body2: 30.nsp,
+    FontSize.body3: 28.nsp,
+    FontSize.body4: 26.nsp,
+    FontSize.body5: 24.nsp,
+    FontSize.tiny1: 22.nsp,
+  };
+
+  static Map<SubpingFontWeight, FontWeight> fontWeightSet = {
+    SubpingFontWeight.bold: FontWeight.bold,
+    SubpingFontWeight.medium: FontWeight.w500,
+    SubpingFontWeight.regular: FontWeight.w400
+  };
+
+  const SubpingText(
+    @required this.text, {
+    @required this.size,
+    Key key,
+    this.color,
+    this.fontWeight,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return (SubText.Text(
+    return (Text(
       text,
-      style: textStyle,
+      style: TextStyle(
+          fontSize: fontSizeSet[this.size],
+          color: colorSet[this.color],
+          fontWeight: fontWeightSet[this.fontWeight]),
     ));
   }
 }
