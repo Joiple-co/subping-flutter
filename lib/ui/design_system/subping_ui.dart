1b3305a3e12dfd6572ac84375b4ce79fe9a6847f
W1nU <jsw9808@gmail.com>
2021-07-02 14:08:50 +0900

    수정 : 디자인시스템 import 버그 수정

--- lib/ui/design_system/subping_ui.dart
+++ lib/ui/design_system/subping_ui.dart
@@ -3,7 +3,7 @@ library subping_ui;
 export './button/sqaure_button.dart';
 export './page/header_safe.dart';
 export './page/horizontal_padding.dart';
-export './textfield/widthfit_textfield.dart';
+export './textfield/textfield.dart';
 export './transition/circlular_reveal_transition.dart';
 export './space/space.dart';
 export './settingTable.dart';
