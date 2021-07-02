84772e13573093fe6a54b9571057a4e14a40b39d
W1nU <jsw9808@gmail.com>
2021-07-02 20:41:42 +0900

    수정 : Secure 필요없는 모듈 제거

--- lib/modules/secure/secure.dart
+++ lib/modules/secure/secure.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:amplify_api/amplify_api.dart';
-import 'package:amplify_flutter/amplify.dart';
 import 'package:crypton/crypton.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:subping/model/rsa_model.dart';
