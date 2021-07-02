04aaea8a80149c5c76a0623121d7322106335c41
W1nU <jsw9808@gmail.com>
2021-07-02 17:16:32 +0900

    기능 : API 모듈 post 추가, header 변경 및 packageInfo 업그레이드

--- lib/modules/api/api.dart
+++ lib/modules/api/api.dart
@@ -1,49 +1,78 @@
-import 'dart:convert';
-import 'dart:io';
+import "dart:convert";
+import "dart:io";
+import 'dart:typed_data';
 
-import 'package:amplify_api/amplify_api.dart';
-import 'package:amplify_flutter/amplify.dart';
-import 'package:device_info/device_info.dart';
-import 'package:package_info/package_info.dart';
-import 'package:subping/modules/cognito/cognito.dart';
+import "package:amplify_api/amplify_api.dart";
+import "package:amplify_flutter/amplify.dart";
+import "package:device_info/device_info.dart";
+import "package:package_info/package_info.dart";
+import "package:subping/modules/cognito/cognito.dart";
 
 class API {
   static final JSON = JsonEncoder();
 
-  static Future<RestResponse> get(String service, String path) async {
-    final PackageInfo packageInfo = PackageInfo();
+  static Future<Map<String, String>> _makeHeader() async {
+    Map<String, String> header = {};
+    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
     final String appVersion = packageInfo.version;
     final String buildNumber = packageInfo.buildNumber;
 
-    dynamic deviceInfo;
     String deviceName;
     String deviceVersion;
     String deviceId;
 
     if (Platform.isAndroid) {
-      deviceInfo = await deviceInfoPlugin.androidInfo;
+      AndroidDeviceInfo deviceInfo = await deviceInfoPlugin.androidInfo;
       deviceName = deviceInfo.model;
       deviceVersion = deviceInfo.version.toString();
       deviceId = deviceInfo.androidId;
     } else {
-      deviceInfo = await deviceInfoPlugin.iosInfo;
+      IosDeviceInfo deviceInfo = await deviceInfoPlugin.iosInfo;
       deviceName = deviceInfo.model;
       deviceVersion = deviceInfo.systemVersion;
       deviceId = deviceInfo.identifierForVendor;
     }
 
-    try {
-      RestOptions options = RestOptions(path: path, headers: {
-        "deviceId": deviceId
-      });
-      RestOperation restOperation = Amplify.API.get(restOptions: options);
-      
-      return restOperation.response;
-    } on ApiException catch(e) {
-      print("GET call failed: $e");
-    }
-   
+    header = {
+      "deviceName": deviceName,
+      "deviceVersion": deviceVersion,
+      "buildNumber": buildNumber,
+      "appVersion": appVersion,
+      "deviceId": deviceId,
+      "Content-Type": "application/json; charset=utf-8"
+    };
+
+    return header;
   }
 
+  static Future<RestResponse> get(String service, String path) async {
+    Map<String, String> header = await _makeHeader();
+
+    try {
+      RestOptions options = RestOptions(path: path, headers: header);
+      RestOperation restOperation = Amplify.API.get(restOptions: options);
+
+      return restOperation.response;
+    } on ApiException catch (e) {
+      print("GET call failed: $e");
+    }
+  }
+
+  static Future<RestResponse> post(
+      String service, String path, {Map<dynamic, dynamic> body}) async {
+    Map<String, String> header = await _makeHeader();
+
+    try {
+      RestOptions options = RestOptions(
+          path: path,
+          headers: header,
+          body: Uint8List.fromList(jsonEncode(body).codeUnits));
+      RestOperation restOperation = Amplify.API.post(restOptions: options);
+      
+      return restOperation.response;
+    } on ApiException catch (e) {
+      print("Post call failed: $e");
+    }
+  }
 }
