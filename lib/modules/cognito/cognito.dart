99ea56a0d5561ac1fa3aa4782c6672e79fc0b939
W1nU <jsw9808@gmail.com>
2021-07-02 18:20:46 +0900

    기능 : Cognito 모듈 SignUpStart 구현

--- lib/modules/cognito/cognito.dart
+++ lib/modules/cognito/cognito.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:subping/amplifyconfiguration.dart';
 import 'package:subping/model/body_model.dart';
 import 'package:subping/modules/api/api.dart';
+import 'package:subping/modules/error_handler/error_handler.dart';
 import 'package:subping/modules/secure/secure.dart';
 
 
@@ -29,10 +30,26 @@ class Cognito {
     return _awsConfig['api']['plugins']['awsAPIPlugin']['auth']['endpoint'];
   }
 
-  Future<BodyModel> signUpStart(String email, String password) async {
+  void signUpStart(String email, String password) async {
     final secure = Secure();
     final encryptedPassword = await secure.encrpytRSA(password);
-  }
+    password = "";
+
+    final rawResponse = await API.post("auth", "/signUpStart", body: {
+      "email" : email, "password": encryptedPassword
+    });
+
+    Map<String, dynamic> response = jsonDecode(new String.fromCharCodes(rawResponse.data));
+    BodyModel data = BodyModel.fromJson(response);
+    print(data.message);
+    if(data.success) {
+    
+    }
+
+    else {
+      ErrorHandler.errorHandler("default");
+    }
+  } 
 
   Future<BodyModel> isDuplicateEmail(String email) async {
     final rawResponse = await API.post("auth", "/emailDuplicate", body: {
@@ -44,16 +61,3 @@ class Cognito {
     return body;
   }
 }
-
-// Future<BodyModel> signUpStart(String email, String password) async {
-//     final secure = Secure();
-//     password = "";
-
-//     final response = await API.post("auth", "/signUpStart",
-//         body: {"email": email, "password": encryptedPassword}, withAuth: false);
-
-//     final jsonBody = jsonDecode(response.body);
-//     BodyModel body = BodyModel.fromJson(jsonBody);
-
-//     return body;
-//   }
