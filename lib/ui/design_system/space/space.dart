96bfa69cb1ce9fdc89a523806a5c30958f44e8f0
W1nU <jsw9808@gmail.com>
2021-07-05 15:38:59 +0900

    수정 : 디자인시스템 Size -> SubpingSize

--- lib/ui/design_system/space/space.dart
+++ lib/ui/design_system/space/space.dart
@@ -3,22 +3,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../settingTable.dart';
 
 class Space extends StatelessWidget {
-  final Size size;
+  final SubpingSize size;
 
   const Space({@required this.size, Key key}) : super(key: key);
 
-  static Map<Size, Map> table = {
-    Size.large64: {"width": 64.w, "height": 64.h},
-    Size.large50: {"width": 50.w, "height": 50.h},
-    Size.large48: {"width": 48.w, "height": 48.h},
-    Size.large40: {"width": 40.w, "height": 40.h},
-    Size.large30: {"width": 30.w, "height": 30.h},
-    Size.medium24: {"width": 24.w, "height": 24.h},
-    Size.medium20: {"width": 20.w, "height": 20.h},
-    Size.medium16: {"width": 16.w, "height": 16.h},
-    Size.tiny14: {"width": 14.w, "height": 14.h},
-    Size.tiny12: {"width": 12.w, "height": 12.h},
-    Size.tiny10: {"width": 10.w, "height": 10.h},
+  static Map<SubpingSize, Map> table = {
+    SubpingSize.large64: {"width": 64.w, "height": 64.h},
+    SubpingSize.large50: {"width": 50.w, "height": 50.h},
+    SubpingSize.large48: {"width": 48.w, "height": 48.h},
+    SubpingSize.large40: {"width": 40.w, "height": 40.h},
+    SubpingSize.large30: {"width": 30.w, "height": 30.h},
+    SubpingSize.medium24: {"width": 24.w, "height": 24.h},
+    SubpingSize.medium20: {"width": 20.w, "height": 20.h},
+    SubpingSize.medium16: {"width": 16.w, "height": 16.h},
+    SubpingSize.tiny14: {"width": 14.w, "height": 14.h},
+    SubpingSize.tiny12: {"width": 12.w, "height": 12.h},
+    SubpingSize.tiny10: {"width": 10.w, "height": 10.h},
   };
 
   @override
