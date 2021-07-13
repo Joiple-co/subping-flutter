a3d00e55b1f9f7c40f13ab04d2f4e126be46a071
W1nU <jsw9808@gmail.com>
2021-07-14 00:22:44 +0900

    기능 : 디자인시스템 TitleAppBar 추가

---
+++ lib/ui/design_system/appbar/title_appbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:subping/ui/design_system/subping_ui.dart';
+
+class TitleAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final String title;
+  final bool hasBackButton;
+  final Size preferredSize;
+
+  TitleAppBar(this.title, {this.hasBackButton = false, Key key})
+      : preferredSize = Size.fromHeight(100.0.h),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: SubpingColor.white100,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        collapseMode: CollapseMode.pin,
+      ),
+      centerTitle: false,
+      leading: hasBackButton
+          ? IconButton(
+              onPressed: Get.back,
+              icon: Icon(
+                Icons.chevron_left,
+                size: 60.nsp,
+                color: SubpingColor.black100,
+              ))
+          : null,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        SubpingText(title,
+            color: SubpingColor.black100,
+            fontWeight: SubpingFontWeight.bold,
+            size: SubpingFontSize.title5),
+      ]),
+      leadingWidth: hasBackButton ? null : 0.w,
+      elevation: 0,
+    );
+  }
+}
