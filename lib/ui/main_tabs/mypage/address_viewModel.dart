2a65803e0c1c51ea0ebf8a2676db7d7f0ac2e4a8
Crabee <37769573+pqr4579@users.noreply.github.com>
2021-07-16 17:20:29 +0900

    기능 : 주소입력 (#16)
    * 기능 : size 56 추가 , 주소 입력 페이지 생성
    
    * 기능 : Adress 프론트 작업 완료 및 API 연동

---
+++ lib/ui/main_tabs/mypage/address_viewModel.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kopo/kopo.dart';
+import 'package:subping/model/address_model.dart';
+import 'package:subping/modules/error_handler/error_handler.dart';
+import 'package:subping/repository/address_repository.dart';
+
+class AddressViewModel extends GetxController {
+  String zipCode = "";
+  String address = "";
+  String detailAddress = "";
+
+  FocusNode detailedAddressFocusNode = FocusNode();
+  TextEditingController zipCodeController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  void routingKopo(BuildContext context) async {
+    await Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => Kopo(
+              callback: (kopoModel) => setAddressInform(kopoModel),
+            )));
+  }
+
+  void setAddressInform(KopoModel kopoModel) {
+    zipCode = kopoModel.zonecode;
+    address = kopoModel.address;
+
+    zipCodeController.text = kopoModel.zonecode;
+    addressController.text = kopoModel.address;
+
+    detailedAddressFocusNode.requestFocus();
+  }
+
+  void onChangeDetailAddress(String text) {
+    detailAddress = text;
+  }
+
+  bool checkValid() {
+    return (zipCode.length == 0 ||
+        zipCode.length == 0 ||
+        detailAddress.length < 4);
+  }
+
+  void onSubmit() async {
+    AddressRepository _addressRepository = AddressRepository();
+
+    if (checkValid()) {
+      print("주소 입력 똑바로 해주세요");
+    } else {
+      try {
+        final response = await _addressRepository.makeAddress(AddressModel(
+            address: addressController.text,
+            postCode: zipCodeController.text,
+            detailedAddress: detailAddress,
+            addressName: "집",
+            isDefault: true));
+
+        if (!response.success) {
+          ErrorHandler.errorHandler(response.message);
+        } else {
+          Get.back();
+        }
+      } catch (e) {
+        ErrorHandler.errorHandler("defalt");
+        print(e);
+      }
+    }
+  }
+}
