2610ac3df4107315e54237f915f11a5893937fe3
Lee Jeong Jin <dlwjdwls6504@gmail.com>
2021-07-26 14:27:27 +0900

    수정 : chart 뷰 bug fix

--- lib/ui/main_tabs/home/chart.dart
+++ lib/ui/main_tabs/home/chart.dart
@@ -14,64 +14,67 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     return HorizontalPadding(
       child: Container(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text.rich(SubpingTextSpan(children: [
-                  SubpingTextSpan(
-                    text: "인기차트",
-                    fontSize: SubpingFontSize.title6,
-                    fontWeight: SubpingFontWeight.bold,
+                Row(
+                  children: [
+                    Text.rich(SubpingTextSpan(children: [
+                      SubpingTextSpan(
+                        text: "인기차트",
+                        fontSize: SubpingFontSize.title6,
+                        fontWeight: SubpingFontWeight.bold,
+                      ),
+                    ])),
+                    Space(size: SubpingSize.tiny10),
+                    Container(
+                        padding: EdgeInsets.fromLTRB(15.w, 8.h, 15.w, 8.h),
+                        decoration: BoxDecoration(),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 27.w,
+                              height: 27.h,
+                              child: Image.asset(
+                                "assets/icon/clock.png",
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(10.w, 0, 0, 5.h),
+                              child: SubpingText(hotChartData?.standardTime,
+                                  fontWeight: SubpingFontWeight.medium,
+                                  size: SubpingFontSize.body3,
+                                  color: SubpingColor.black60),
+                            )
+                          ],
+                        ))
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () => Get.toNamed("/hotChart"),
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(SubpingSize.medium20.w, 5.h,
+                        SubpingSize.medium20.w, 5.h),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: SubpingColor.back20),
+                    child: SubpingText(
+                      '더보기',
+                      color: SubpingColor.subping50,
+                      size: SubpingFontSize.body3,
+                      fontWeight: SubpingFontWeight.bold,
+                    ),
                   ),
-                ])),
-
-                Space(size: SubpingSize.tiny10),
-                Container(
-                    padding: EdgeInsets.fromLTRB(15.w, 8.h, 15.w, 8.h),
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 27.w,
-                          height: 27.h,
-                          child: Image.asset(
-                            "assets/icon/clock.png",
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(10.w, 0, 0, 5.h),
-                          child: SubpingText(hotChartData?.standardTime,
-                              fontWeight: SubpingFontWeight.medium,
-                              size: SubpingFontSize.body3,
-                              color: SubpingColor.black60),
-                        )
-                      ],
-                    ))
+                ),
               ],
             ),
-            GestureDetector(
-              onTap: () => Get.toNamed("/hotChart"),
-              child: Container(
-                padding: EdgeInsets.fromLTRB(
-                    SubpingSize.medium20.w, 5.h, SubpingSize.medium20.w, 5.h),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: SubpingColor.back20),
-                child: SubpingText(
-                  '더보기',
-                  color: SubpingColor.subping50,
-                  size: SubpingFontSize.body3,
-                  fontWeight: SubpingFontWeight.bold,
-                ),
-              ),
-            ),
+            Space(size: SubpingSize.large40),
             Container(
               child: Column(
                   children: List.generate(limitItem, (index) {
@@ -88,7 +91,7 @@ class Chart extends StatelessWidget {
             )
           ],
         ),
-      ])),
+      ),
     );
   }
 }
