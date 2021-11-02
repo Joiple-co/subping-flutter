969a89e2c7b4b1e32f466d1bc2e3ad3ebb1cb727
Gwangi Park <qkrrhkddl7@naver.com>
2021-11-02 16:49:36 +0900

    search repository 수정

--- lib/repository/search_repository.dart
+++ lib/repository/search_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:subping/model/auto_complete_model.dart';
 import 'package:subping/model/body_model.dart';
 import 'package:subping/model/search_model.dart';
 import 'package:subping/modules/api/api.dart';
@@ -13,12 +14,26 @@ class SearchRepository {
 
       final decodedResponse = utf8.decode(rawResponse.data);
       BodyModel response = BodyModel.fromJson(jsonDecode(decodedResponse));
-      print(response.message);
-
       return SearchModel.fromJson(response.message);
     } catch (e) {
       print(e);
       return SearchModel();
     }
   }
+
+    Future<AutoCompleteModel> getAutoComplete(String requestWord) async {
+    try {
+      final rawResponse = await API.post("service", "/autoComplete", body: {
+        "requestWord": requestWord
+      });
+      final decodedResponse = utf8.decode(rawResponse.data);
+      BodyModel response = BodyModel.fromJson(jsonDecode(decodedResponse));
+      print(response.message);
+      return AutoCompleteModel.fromJson(response.message);
+    } catch (e) {
+      print(e);
+      return AutoCompleteModel();
+    }
+  }
+
 }
