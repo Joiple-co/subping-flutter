c084422f133e0c9571c60705720f8b86a8fa576a
Gwangi Park <qkrrhkddl7@naver.com>
2021-11-02 17:12:04 +0900

    search 자동완성 model 수정

---
+++ lib/model/auto_complete_model.dart
@@ -0,0 +1,35 @@
+import 'package:subping/model/service_model.dart';
+
+
+class AutoCompleteModel {
+  List<TagResult> tagResult = [];
+  List<ServiceResult> serviceResult = [];
+
+  AutoCompleteModel({ this.serviceResult, this.tagResult });
+
+  AutoCompleteModel.fromJson(Map<String, dynamic> json) {
+
+    json['serviceResult'].forEach((item) {
+      serviceResult.add(ServiceResult.fromJson(item));
+    });
+    
+    json['tagResult'].forEach((item) {
+      tagResult.add(TagResult.fromJson(item));
+    });
+
+  }
+}
+
+class TagResult {
+  String tag;
+
+  TagResult.fromJson(Map<String, dynamic> json) {
+    tag = json['tag'];
+  }
+}
+
+class ServiceResult extends ServiceModel {
+  ServiceResult.fromJson(Map<String, dynamic> json) {
+    super.updateServiceModel(ServiceModel.fromJson(json));
+  }
+}   
