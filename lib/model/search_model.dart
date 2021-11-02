a5625ad1995444a2812cf0ee60d4948d44a42658
Gwangi Park <qkrrhkddl7@naver.com>
2021-11-02 16:46:43 +0900

    search model 수정

--- lib/model/search_model.dart
+++ lib/model/search_model.dart
@@ -1,51 +1,46 @@
+//import 'dart:developer';
+
 import 'package:subping/model/service_model.dart';
 
 class SearchModel {
-  List<TagResult> tagResult = [];
   List<ServiceResult> serviceResult = [];
 
-  SearchModel({this.tagResult, this.serviceResult});
+  SearchModel({ this.serviceResult});
 
   SearchModel.fromJson(Map<String, dynamic> json) {
-    json['tagResult'].forEach((item) {
-      tagResult.add(TagResult.fromJson(item));
-    });
-
+    
     json['serviceResult'].forEach((item) {
       serviceResult.add(ServiceResult.fromJson(item));
     });
-
-    print(tagResult);
     print(serviceResult);
   }
 }
 
-class SearchProperty {
-  int matchedIndex;
-  int length;
-
-  SearchProperty.fromJson(Map<String, dynamic> json) {
-    matchedIndex = json["matchedIndex"];
-    length = json["length"];
-  }
-}
-
-class TagResult {
-  String tag;
-  SearchProperty search;
-
-  TagResult.fromJson(Map<String, dynamic> json) {
-    tag = json['tag'];
-    search = SearchProperty.fromJson(json['search']);
-  }
-}
-
 class ServiceResult extends ServiceModel {
-  SearchProperty search;
+  //SearchProperty search;
 
   ServiceResult.fromJson(Map<String, dynamic> json) {
     super.updateServiceModel(ServiceModel.fromJson(json));
-    search = SearchProperty.fromJson(json['search']);
+   // search = SearchProperty.fromJson(json['search']);
   }
 } 
- 
+
+// class SearchProperty {
+//   int matchedIndex;
+//   int length;
+
+//   SearchProperty.fromJson(Map<String, dynamic> json) {
+//     matchedIndex = json["matchedIndex"];
+//     length = json["length"];
+//   }
+// }
+
+// class ReviewResult {
+//   String tag;
+//   SearchProperty search;
+
+//   ReviewResult.fromJson(Map<String, dynamic> json) {
+//     tag = json['tag'];
+//     search = SearchProperty.fromJson(json['search']);
+//   }
+// }
