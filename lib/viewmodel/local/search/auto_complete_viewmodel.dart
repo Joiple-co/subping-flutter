c5363b9f3a98099f748bb4c2af7cc94c974a9889
Gwangi Park <qkrrhkddl7@naver.com>
2021-11-02 16:41:57 +0900

    search viewmodel

---
+++ lib/viewmodel/local/search/auto_complete_viewmodel.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:subping/model/auto_complete_model.dart';
+import 'package:subping/repository/search_repository.dart';
+
+
+class AutoCompleteViewModel extends GetxController {
+  SearchRepository _searchRepository = SearchRepository();
+  RxString _autoCompleteText = "".obs;
+  Rx<AutoCompleteModel> _autoCompleteResult = AutoCompleteModel().obs;
+  TextEditingController _autoCompleteTextEditingController = TextEditingController();
+  RxBool isFocused = false.obs;
+  //내가 추가해준거 (아래)
+  RxMap<int, int> autoCompleteTextProperty = <int, int>{}.obs;
+
+
+  
+  @override
+  void onInit() {
+    super.onInit();
+    debounce(_autoCompleteText, (String text) async {
+      if(text.length != 0) {
+        _autoCompleteResult .value = await _searchRepository.getAutoComplete(text);
+        _autoCompleteResult .refresh();
+      } else {
+        _autoCompleteResult.value = AutoCompleteModel();
+        _autoCompleteResult .refresh();
+      }
+    },
+    time: Duration(seconds: 1));
+  }
+
+  void onChangeAutoCompleteText(String text) {
+    _autoCompleteText.value = text;
+  }
+
+  void onClickClear() {
+    _autoCompleteText.value = "";
+    _autoCompleteTextEditingController.clear();
+  }
+
+  AutoCompleteModel get autoCompleteResult {
+    return _autoCompleteResult.value;
+  }
+
+  TextEditingController get autoCompleteTextEditingController {
+    return _autoCompleteTextEditingController;
+  }
+
+  String get autoCompleteText {
+    return _autoCompleteText.value;
+  }
+
+  void getAutoCompleteTextProperty(){
+      int matchedIndex=
+      _autoCompleteResult.value.serviceResult[0].name.indexOf(_autoCompleteText);
+  
+}
+
+}
--- lib/viewmodel/local/search/search_viewmodel.dart
+++ lib/viewmodel/local/search/search_viewmodel.dart
@@ -1,26 +1,30 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:subping/model/auto_complete_model.dart';
 import 'package:subping/model/search_model.dart';
 import 'package:subping/repository/search_repository.dart';
 
 class SearchViewModel extends GetxController {
   SearchRepository _searchRepository = SearchRepository();
   RxString _searchText = "".obs;
-  Rx<SearchModel> _searchResult = SearchModel().obs;
+  Rx<AutoCompleteModel> _autoCompleteResult = AutoCompleteModel().obs;
   TextEditingController _searchTextEditingController = TextEditingController();
-
+  RxBool isFocused = false.obs;
+  
   @override
   void onInit() {
     super.onInit();
 
     debounce(_searchText, (String text) async {
       if(text.length != 0) {
-        _searchResult.value = await _searchRepository.getSearch(text);
-        _searchResult.refresh();
+        _autoCompleteResult.value = await _searchRepository.getAutoComplete(text);
+        _autoCompleteResult.refresh();
+        //print(_autoCompleteResult.value.tagResult[0].tag);
+       // print(_autoCompleteResult.value.serviceResult[0]);
       } else {
-        _searchResult.value = SearchModel();
-        _searchResult.refresh();
+        _autoCompleteResult.value = AutoCompleteModel();
+        _autoCompleteResult.refresh();
       }
     },
     time: Duration(seconds: 1));
@@ -35,10 +39,11 @@ class SearchViewModel extends GetxController {
     _searchTextEditingController.clear();
   }
 
-  SearchModel get searchResult {
-    return _searchResult.value;
+  AutoCompleteModel get autoCompleteResult {
+    return _autoCompleteResult.value;
   }
-
+  //위에는 자동완성 관련 데이터 만들었으니 이자리에는 결과페이지에 필요한 데이터 받는걸로
+  
   TextEditingController get searchTextEditingController {
     return _searchTextEditingController;
   }
